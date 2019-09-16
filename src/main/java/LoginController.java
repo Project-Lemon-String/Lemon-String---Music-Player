af6a2a59e4a4c0c57e8e9d66b9e36acfe240caac
Bryan Rojas <bryroj14@gmail.com>
2019-09-16 01:03:18 -0700

    Updated with Login, Signup, Home Views.

---
+++ src/main/java/LoginController.java
@@ -0,0 +1,54 @@
+
+import javafx.application.Application;
+import javafx.fxml.Initializable;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.*;
+
+public class LoginController implements Initializable {
+
+    @FXML
+    private TextField tf_username;
+
+    @FXML
+    private PasswordField tf_password;
+
+    @FXML
+    void login(MouseEvent event) {
+        //if(tf_username.getText() == "Bryan"){
+        //    if(tf_password.getText() == "password"){
+            /*
+            Parent root = FXMLLoader.load(getClass().getResource("/home.fxml"));
+            Node node = (Node) event.getSource();
+            Stage stage = (Stage) node.getScene().getWindow();
+            stage.setScene(new Scene(root));
+            */
+        //    }
+        //}
+    }
+
+    @FXML
+    void signup(MouseEvent event){
+        /*
+        Parent root = FXMLLoader.load(getClass().getResource("/signup.fxml"));
+        Node node = (Node) event.getSource();
+        Stage stage = (Stage) node.getScene().getWindow();
+        stage.setScene(new Scene(root));
+        */
+    }
+
+    public void initialize(URL location, ResourceBundle resources){
+
+    }
+
+}
